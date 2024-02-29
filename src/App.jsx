1edd3a1e61f766f1d61a52fac4be5b52c548895d
Islomjon0221 <islomjon20110221@gmail.com>
2024-02-29 06:17:19 +0500

    iyga vazifa tayyor

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,30 @@
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect,  useState } from 'react';
+import { useEffect, useRef,  useState } from 'react';
 import { getData } from './redux/Weather';
 
 function App() {
   const [inputValue, setInputValue] = useState();
-  const [data, setData] = useState([]);
+  const nameRef = useRef('')
   const weather = useSelector(state => state);
   const dispatch = useDispatch()
   
   useEffect(() => {
-    dispatch(getData(inputValue))
-  }, [])
+    dispatch(getData(inputValue || "fergana"));
+    setInputValue(nameRef.current.value)
+  },[nameRef.current.value, nameRef, inputValue]);
   
-  console.log(16, weather.weather);
-  const handleChange = (e) => {   
-    setInputValue(e.target.value);
-  };
+  // console.log(18, weather.weather);
+  // console.log(19, inputValue);
+  // const handleChange = (e) => {   
+  //   setInputValue(e.target.value);
+  // };
   
   function handleSubmit(e) {
-    // const user = {
-    //   name: weather.weather.weather.name,
-    //   status: weather.weather.status,
-    //   country: weather.weather.weather.sys.country,
-    //   gradus: weather.weather.weather.main.temp,
-    //   namlik: weather.weather.weather.main.humidity,
-    //   havo: weather.weather.weather.main.pressure,
-    //   shamol: weather.weather.weather.wind.speed
-    // }
     e.preventDefault()
-    // setData(user)
-    console.log(19, inputValue);
-    console.log(15, data);
+    setInputValue(nameRef.current.value)
+    // console.log(23, nameRef.current.value);
+
   }
 
 
@@ -40,20 +33,23 @@ function App() {
   return (
     <div className='container'>
       <form onSubmit={handleSubmit} className="wrapper">
-        <input value={inputValue} onChange={handleChange} placeholder="Joy nomini kiriting...." className="name" type="text" />
-      { weather.weather.status == "succes" && <>
+        <input
+        //  value={inputValue} onChange={handleChange}
+        ref={nameRef}
+          placeholder="Joy nomini kiriting...." className="name" type="text" />
+      { weather.weather.status == "success" &&  <>
       <h1>{weather.weather.weather.name},{weather.weather.weather.sys.country}</h1>
         <div className="weather">
          <h3>Gradus      <span>{weather.weather.weather.main.temp}°C</span></h3>
         <h3>Namlik      <span>{weather.weather.weather.main.humidity}%</span></h3>
         <h3>Havo bosimi     <span>{weather.weather.weather.main.pressure}hPa</span></h3>
         <h3>Shamol      <span>{weather.weather.weather.wind.speed}mph</span></h3> 
-        </div>
-        </>
+        </div></>
     }
     <button className="btn">Qidirish</button>
     </form>
     </div>
+    
   )
 }
 
--- src/redux/Weather.js
+++ src/redux/Weather.js
@@ -1,41 +1,39 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+const KEY = '56ff80ac3a0c0f5f2e37a0de8ad98800'
+
 export const getData = createAsyncThunk(
     "getWeather",
-    async (place = "fergana") => {
-        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=56ff80ac3a0c0f5f2e37a0de8ad98800`)
-        .then(res =>  
-            res.json()
-            // setLoading(false)
-            )
-        
+    async (place) => {
+        const base = "https://api.openweathermap.org/data/2.5/weather"
+        const query = `?q= ${place}&units=metric&appid=${KEY}`
+
+    return await fetch(base + query)
+      .then(res => res.json())
         .catch(err => {
-            console.log(err);
+          console.log(err);
         })
     }
-)
+);
 
 export const weatherSlice = createSlice({
     name: "weather",
-    initialState: {data: [], status: ""},
+    initialState: { weather: {}, status: "" },
     reducers: {},
     extraReducers: (builder) => {
-        builder
+      builder
         .addCase(getData.pending, (state) => {
-            state.status ="pending"
-        } )
-        
-        .addCase(getData.fulfilled, (state,actions) => {
-            state.weather = actions.payload
-            state.status ="succes"
-        } )
-
-        
+          state.status = "pending";
+        })
+        .addCase(getData.fulfilled, (state, action) => {
+          state.weather = action.payload;
+          state.status = "success";
+        })
         .addCase(getData.rejected, (state) => {
-            state.status ="rejected"
-        } )
-    }
-})
+          state.status = "rejected";
+        });
+    },
+  });
 
 export default weatherSlice.reducer;
 
